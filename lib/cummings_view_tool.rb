6018a7fc8767894a5039fdd30ecb5e8015f1c62d
Mac <mac@MacBook-Pro.ad.uvu.edu>
2017-10-24 14:26:00 -0600

    Practice gem initial commit

---
+++ lib/cummings_view_tool.rb
@@ -0,0 +1,7 @@
+require "cummings_view_tool/version"
+require "cummings_view_tool/renderer"
+
+
+module CummingsViewTool
+  # Your code goes here...
+end
---
+++ lib/cummings_view_tool/version.rb
@@ -0,0 +1,3 @@
+module CummingsViewTool
+  VERSION = "0.1.0"
+end
